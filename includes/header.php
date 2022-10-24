82e7104acbd9d95ecbfcb3a972908022a2ee988f
mk346 <calebkinyuamunene@gmail.com>
2022-10-24 10:48:41 +0300

    #update4

--- includes/header.php
+++ includes/header.php
@@ -2,6 +2,8 @@
 require 'config/config.php';
 if (isset($_SESSION['username'])) {
     $userLoggedIn = $_SESSION['username'];
+    $user_details_query = mysqli_query($con, "SELECT * FROM users WHERE username='$userLoggedIn'");
+    $user = mysqli_fetch_array($user_details_query);
 } else {
     header("Location: register.php");
 }
@@ -15,13 +17,28 @@ if (isset($_SESSION['username'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
     <script src="../assets/js/bootstrap.js"></script>
-    <link rel="stylesheet" href="../assets/css/bootstrap.css">
-    <link rel="stylesheet" href="../assets/css/style.css">
+    <script src="https://kit.fontawesome.com/0fe3bc1f22.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
     <title>Home Page</title>
 </head>
+
 <body>
     <div class="top_bar">
         <div class="logo">
             <a href="index.php">My Chat Space</a>
         </div>
+        <nav>
+            <a href="#">
+                <?php echo $user['first_name'];?>
+            </a>
+            <a href="index.php"><i class="fa fa-home fa-lg"></i></a>
+            <a href="#"><i class="fa fa-envelope fa-user fa-lg"></i></a>
+            <a href="#"><i class="fa fa-bell-o fa-lg"></i></a>
+            <a href="#"><i class="fa fa-users fa-lg"></i></a>
+            <a href="#"><i class="fa fa-cog fa-lg"></i></a>
+
+
+        </nav>
     </div>
+    <div class="wrapper">
--- index.php
+++ index.php
@@ -1,7 +1,23 @@
-<?php 
-include ("includes/header.php");
+<?php
+include("includes/header.php");
 // session_destroy();
 ?>
-    <h1>Welcome</h1>
+<div class="user_details column">
+    <a href="#"><img src="<?php echo $user['profile_pic']; ?>" alt=""></a>
+    <div class="user_details_left_right">
+        <a href="#">
+            <?php
+            echo $user['first_name'] . " " . $user['last_name'];
+            ?>
+        </a>
+        <br>
+        <?php echo "Posts: " . $user['num_posts'] . "<br>";
+        echo "Likes: " . $user['num_likes'];
+        ?>
+    </div>
+
+</div>
+</div>
 </body>
+
 </html>
